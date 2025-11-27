e439b598db1341f56a9f85576e1e6dff23fa0904
irenegutrabo <irenegutrabo@uma.es>
2025-11-27 09:17:44 +0100

    Nuevo proyecto

---
+++ redia/src/main/java/com/redia/redia/RediaApplication.java
@@ -0,0 +1,13 @@
+package com.redia.redia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediaApplication.class, args);
+	}
+
+}
---
+++ redia/src/test/java/com/redia/redia/RediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redia.redia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
